84d217419333a2c1c8cb09e02a0e09425c023a4e
Charles-Antoine Francisco <charlantfr@gmail.com>
2018-04-10 11:27:55 -0400

    Correlation and Regression Lines #2 and #3

---
+++ ai/machine-learning/correlation-regression-lines3/main.py
@@ -0,0 +1,21 @@
+import math
+
+# Reference: https://tinyurl.com/y9mrq7dr
+# y = mx + b
+
+X = [15, 12, 8, 8, 7, 7, 7, 6, 5, 3]
+Y = [10, 25, 17, 11, 13, 17, 20, 13, 9, 15]
+
+n = len(X)
+sum_x = sum(X)
+sum_y = sum(Y)
+sum_x2 = sum(i * i for i in X)
+sum_xy = sum(i * j for i, j in zip(X, Y))
+
+b = (sum_y * sum_x2 - sum_x * sum_xy) / (n * sum_x2 - sum_x ** 2)
+m = (n * sum_xy - sum_x * sum_y) / (n * sum_x2 - sum_x ** 2)
+
+x = 10
+y = m * x + b
+
+print(round(y, 1))
